c0b88ff7a846fe3ad488b71cf265c6b44283622f
Valentina <valyaum@rambler.ru>
2016-02-22 16:10:50 +0500

    Завершающее задание. Правка 2.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
   var link = document.querySelector(".map-address .btn");
   var popup = document.querySelector(".modal-content");
   var close = document.querySelector(".modal-content-close");
-      var login = popup.querySelector("[name=login]");
+  var login = popup.querySelector("[name=login]");
 
-      link.addEventListener("click", function(event) {
+    link.addEventListener("click", function(event) {
         event.preventDefault();
         popup.classList.add("modal-content-show");
         login.focus();
       });
 
-      close.addEventListener("click", function(event) {
+    close.addEventListener("click", function(event) {
         event.preventDefault();
         popup.classList.remove("modal-content-show");
       });
